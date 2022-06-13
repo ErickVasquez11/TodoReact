70cedf0563133e49d08dca10c6544b0195f5955a
ErickVasquez11 <00065520@uca.edu.sv>
2022-06-13 17:09:57 -0600

    Todo List

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import TodoList from  './components/TodoList';
+
+//Creando funcion principal
+function App() {
+  return (
+   <div className='todo-app'> <TodoList /></div>
+  );
+}
+
+export default App;
